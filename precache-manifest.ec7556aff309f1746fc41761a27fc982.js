2d30b4e6164fcb3b3f348435aceacae1df5c2094
t0ri <victoriarosemurray@gmail.com>
2020-03-23 18:56:11 -0700

    Updates

---
+++ precache-manifest.ec7556aff309f1746fc41761a27fc982.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e8e89458dc44cff683009806dd928f31",
+    "url": "/littles-client/index.html"
+  },
+  {
+    "revision": "768b0e214feee11f8b53",
+    "url": "/littles-client/static/css/main.6610f63a.chunk.css"
+  },
+  {
+    "revision": "d3e06301911510225bfc",
+    "url": "/littles-client/static/js/2.9491efa3.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/littles-client/static/js/2.9491efa3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "768b0e214feee11f8b53",
+    "url": "/littles-client/static/js/main.8b5c1b2d.chunk.js"
+  },
+  {
+    "revision": "f54700948daf1704cb5a",
+    "url": "/littles-client/static/js/runtime-main.1fd88d7b.js"
+  }
+]);
