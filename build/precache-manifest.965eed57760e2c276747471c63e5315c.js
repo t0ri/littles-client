3be88713018d58b09506b23fdd0d4f3ce1dc8b74
t0ri <victoriarosemurray@gmail.com>
2020-03-23 18:37:21 -0700

    add build

---
+++ build/precache-manifest.965eed57760e2c276747471c63e5315c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a3cc4cadde40834e5b569c8ffc8b73d6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "768b0e214feee11f8b53",
+    "url": "/static/css/main.6610f63a.chunk.css"
+  },
+  {
+    "revision": "d3e06301911510225bfc",
+    "url": "/static/js/2.9491efa3.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/static/js/2.9491efa3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "768b0e214feee11f8b53",
+    "url": "/static/js/main.8b5c1b2d.chunk.js"
+  },
+  {
+    "revision": "3d6bc2a88b31528baf6a",
+    "url": "/static/js/runtime-main.960988bc.js"
+  }
+]);
